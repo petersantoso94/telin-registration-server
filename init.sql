62b0850d068dd3546f10489126687372851c138f
= <petersantoso@starlordtech.com>
2019-11-08 18:26:47 +0800

    initial commit

---
+++ init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+  ID SERIAL PRIMARY KEY,
+  author VARCHAR(255) NOT NULL,
+  title VARCHAR(255) NOT NULL
+);
+
+INSERT INTO books (author, title)
+VALUES  ('J.K. Rowling', 'Harry Potter');
