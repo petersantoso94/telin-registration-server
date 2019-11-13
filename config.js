46ec4fd8968f06e981ae88ace02b30290541d0de
= <petersantoso@starlordtech.com>
2019-11-13 14:33:21 +0800

    dev testing script

--- config.js
+++ config.js
@@ -4,8 +4,9 @@ const {
     Pool
 } = require('pg')
 const isProduction = process.env.NODE_ENV === 'production'
+const dbHOST = process.env.NODE_ENV === 'development' ? 'localhost' : process.env.DB_HOST
 
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${dbHOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
 const pool = new Pool({
     connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
