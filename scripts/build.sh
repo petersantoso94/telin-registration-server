bdb702c22b655e732ad3265160303af48d8c569c
= <petersantoso@starlordtech.com>
2019-11-13 14:21:20 +0800

    build script

---
+++ scripts/build.sh
@@ -0,0 +1,1 @@
+docker image rm petersantoso/telin-registration-server-app && docker build --no-cache -t petersantoso/telin-registration-server-app:latest ../ && docker push petersantoso/telin-registration-app:latest
