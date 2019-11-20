77812c744243e4e345152a1beb859805577dd5ad
= <petersantoso@starlordtech.com>
2019-11-20 18:26:36 +0800

    update sql for renew the superadmin password

--- sql/addcolumn.sql
+++ sql/addcolumn.sql
@@ -1,8 +1,8 @@
 -- ALTER TABLE admins 
 -- ADD COLUMN country VARCHAR(255);
 
--- UPDATE admins SET country = 'All' WHERE id = 1;
+UPDATE admins SET password = '1bea0074d6f1381fd13269565d91a344' WHERE id = 1;
 
 -- INSERT INTO admins VALUES ('2','adminhk','387018512d52cdb8bb9f3b73b01f8e82','Hongkong'),('3','adminmy','b3b5576463aee382e423d41f03c19d11','Malaysia'),('4','admintw','e4e6be0827323f9c78be2cd32dfb36ab','Taiwan'),('5','adminapps','ec1821911e172d21908035f8f9d29bc9','Apps');
 
-INSERT INTO admins VALUES ('5','adminapps','ec1821911e172d21908035f8f9d29bc9','Apps');
+-- INSERT INTO admins VALUES ('5','adminapps','ec1821911e172d21908035f8f9d29bc9','Apps');
