c2a1f985538fe6d7b5e3444ffca116634697d865
= <petersantoso@starlordtech.com>
2019-11-21 17:52:50 +0800

    run script updated

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,1 +1,1 @@
-docker-compose down && docker image rm petersantoso/telin-registration-server-app -f && docker-compose build --pull && docker-compose up -d
+docker image rm petersantoso/telin-registration-server-app -f && docker-compose build --pull && docker-compose up --build -d
